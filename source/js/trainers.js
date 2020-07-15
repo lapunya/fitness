44131e9f45376e54d48f8d80bd9ee306c86c1a8e
Pavel <pavellapunov@inbox.ru>
2020-07-15 23:14:19 +0300

    feat(trainers) - add swiper

---
+++ source/js/trainers.js
@@ -0,0 +1,28 @@
+'use strict';
+
+var trainersSwiperContainer = document.querySelector('.trainers__swiper-container');
+
+var trainersSwiper = new Swiper(trainersSwiperContainer, {
+  breakpoints: {
+    1199: {
+      slidesPerView: 4,
+      slidesPerGroup: 4,
+      spaceBetween: 40
+    },
+    767: {
+      slidesPerView: 2,
+      slidesPerGroup: 2,
+      spaceBetween: 30
+    },
+    320: {
+      slidesPerView: 1,
+      slidesPerGroup: 1
+    }
+  },
+  slideClass: 'trainers__slide',
+  wrapperClass: 'trainers__wrapper',
+  navigation: {
+    nextEl: '.trainers__button-next',
+    prevEl: '.trainers__button-prev',
+  }
+});
