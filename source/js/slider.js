81de1b60df826fa873fd0bc0bf6ce22cb425ddd9
Pavel <pavellapunov@inbox.ru>
2020-07-15 18:47:51 +0300

    feat(review) - add review classes for swiper

--- source/js/slider.js
+++ source/js/slider.js
@@ -1,9 +1,10 @@
 'use strict';
 
-var slider = document.querySelector('.swiper-container');
+var slider = document.querySelector('.review__swiper-container');
 
-var mySwiper = new Swiper(slider, {
+var reviewSwiper = new Swiper(slider, {
   slidesPerView: 1,
+  slideClass: 'review__slide',
   navigation: {
     nextEl: '.swiper-button-next',
     prevEl: '.swiper-button-prev',
